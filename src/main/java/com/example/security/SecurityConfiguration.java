dfed1cb907623eb58ac847dad7c522625688f388
Dilyan Marinov <dilyan.marinov@markovskisolutions.com>
2016-09-26 17:05:15 +0300

    Added console support

--- src/main/java/com/example/security/SecurityConfiguration.java
+++ src/main/java/com/example/security/SecurityConfiguration.java
@@ -19,11 +19,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
  * @author dilyan
  */
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
-    
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
     @Autowired
     UserDetailsService userDetailsService;
-
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
@@ -34,26 +33,30 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.userDetailsService(userDetailsService()).csrf().disable()
-      .authorizeRequests()
-        .antMatchers(HttpMethod.POST, "/post").access("hasRole('ROLE_BLOGGER')")
-        .antMatchers(HttpMethod.PUT, "/post").access("hasRole('ROLE_BLOGGER')")
-        .antMatchers(HttpMethod.DELETE, "/post").access("hasRole('ROLE_BLOGGER')")
-        .antMatchers(HttpMethod.GET, "/post/**").access("hasAnyRole('ROLE_BLOGGER', 'ROLE_USER')")
-        .antMatchers(HttpMethod.GET, "/posts").access("hasAnyRole('ROLE_BLOGGER', 'ROLE_USER')")
-        .antMatchers(HttpMethod.POST, "/user").access("hasRole('ROLE_ADMIN')")
-        .antMatchers(HttpMethod.PUT, "/user").access("hasRole('ROLE_ADMIN')")
-        .antMatchers(HttpMethod.DELETE, "/user").access("hasRole('ROLE_ADMIN')")
-        .antMatchers(HttpMethod.GET, "/user/**").access("hasAnyRole('ROLE_ADMIN')")
-        .antMatchers(HttpMethod.GET, "/users").access("hasAnyRole('ROLE_ADMIN')")
-        .antMatchers(HttpMethod.POST, "/comment").authenticated()
-        .antMatchers(HttpMethod.PUT, "/comment").authenticated()
-        .antMatchers(HttpMethod.GET, "/comment").authenticated()
-        .antMatchers(HttpMethod.GET, "/comments").authenticated()
-        .antMatchers(HttpMethod.DELETE, "/comment").authenticated()
-        .anyRequest().permitAll()
-        .and()
-      .httpBasic().and()
-      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/post").access("hasRole('ROLE_BLOGGER')")
+                .antMatchers(HttpMethod.PUT, "/post").access("hasRole('ROLE_BLOGGER')")
+                .antMatchers(HttpMethod.DELETE, "/post").access("hasRole('ROLE_BLOGGER')")
+                .antMatchers(HttpMethod.GET, "/post/**").access("hasAnyRole('ROLE_BLOGGER', 'ROLE_USER')")
+                .antMatchers(HttpMethod.GET, "/posts").access("hasAnyRole('ROLE_BLOGGER', 'ROLE_USER')")
+                .antMatchers(HttpMethod.POST, "/user").access("hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.PUT, "/user").access("hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.DELETE, "/user").access("hasRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.GET, "/user/**").access("hasAnyRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.GET, "/users").access("hasAnyRole('ROLE_ADMIN')")
+                .antMatchers(HttpMethod.POST, "/comment").authenticated()
+                .antMatchers(HttpMethod.PUT, "/comment").authenticated()
+                .antMatchers(HttpMethod.GET, "/comment").authenticated()
+                .antMatchers(HttpMethod.GET, "/comments").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/comment").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic().and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers("/console/**").permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 
     @Override
